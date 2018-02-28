15e1d5a4e02108cd02beb58dae91faebb4fd799e
Kyle Nielsen <kylen@uoregon.edu>
2018-02-28 15:24:10 -0800

    Saving work

---
+++ DockerRestAPI/consumer/index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>CIS 322 REST-api: Brevets</title>
+    </head>
+
+    <body>
+        <h1>List of brevets</h1>
+        <ul>
+            <?php
+            $json = file_get_contents('http://api-service/');
+            $obj = json_decode($json);
+	          $brevets = $obj->Laptops; //TODO change to Brevets later
+            foreach ($brevets as $b) {
+                echo "<li>$b</li>";
+            }
+            ?>
+        </ul>
+    </body>
+</html>

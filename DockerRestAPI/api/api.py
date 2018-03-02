1c114ac3e07ed8525e5efd25f414eaeadddd070a
Kyle Nielsen <kylen@uoregon.edu>
2018-03-02 11:22:57 -0800

    Fixed top parameter. Everything working as expected, except csv prints '\n' instead of a new line

--- DockerRestAPI/api/api.py
+++ DockerRestAPI/api/api.py
@@ -1,6 +1,6 @@
 # Laptop Service
 
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 from pymongo import MongoClient
 import os
@@ -13,14 +13,18 @@ client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'], 27017)
 db = client.tododb
 
 class All(Resource):
-    def get(self, top=0):  # defaulting to 0 because if someone wants the first 0 entries, they just won't go to the page
-        if top:
+    def get(self):
+        if request.args.get('top'):  # if 'top' parameter was given
+            top = request.args.get('top')
+            top = int(top)
             data = []
-            for i in range(top):
-                data.append(db.tododb.find_one())
+            data = db.tododb.find().limit(top)
+
         else:
             data = db.tododb.find()
+
         open_close = {}
+
         for pair in data:
             name = pair['name']
             desc = pair['description']
@@ -29,63 +33,114 @@ class All(Resource):
         return open_close
 
 class Open(Resource):
-    def get(self, top=0):  #TODO add arg n
-        data = db.tododb.find()  #TODO only find first n
+    def get(self):
+        if request.args.get('top'):  # if 'top' parameter was given
+            top = request.args.get('top')
+            top = int(top)
+            data = []
+            data = db.tododb.find().limit(top)
+
+        else:
+            data = db.tododb.find()
+
         opens = {}
+
         for pair in data:
             name = pair['name']
             desc = pair['description']
             open_time = desc[0]
             opens[name] = open_time
+
         return opens
 
 class Close(Resource):
-    def get(self, top=0):
-        data = db.tododb.find()
+    def get(self):
+        if request.args.get('top'):  # if 'top' parameter was given
+            top = request.args.get('top')
+            top = int(top)
+            data = []
+            data = db.tododb.find().limit(top)
+
+        else:
+            data = db.tododb.find()
+
         closures = {}
+
         for pair in data:
             name = pair['name']
             desc = pair['description']
             close_time = desc[1]
             closures[name] = close_time
+
         return closures
 
 class AllCSV(Resource):
-    def get(self, top=0):
-        data = db.tododb.find()
+    def get(self):
+        if request.args.get('top'):  # if 'top' parameter was given
+            top = request.args.get('top')
+            top = int(top)
+            data = []
+            data = db.tododb.find().limit(top)
+
+        else:
+            data = db.tododb.find()
+
         csv = "km, open, close\n"  # column header
+
         for pair in data:
             name = pair['name']
             desc = pair['description']
             open_time = desc[0]
             close_time = desc[1]
             csv += name + ', ' + open_time + ',' + close_time + '\n'
+
         return csv
 
 class OpenCSV(Resource):
-    def get(self, top=0):
-        data = db.tododb.find()
+    def get(self):
+        if request.args.get('top'):  # if 'top' parameter was given
+            top = request.args.get('top')
+            top = int(top)
+            data = []
+            data = db.tododb.find().limit(top)
+
+        else:
+            data = db.tododb.find()
+
         opens = "km, open\n"  # column header
+
         for pair in data:
             name = pair['name']
             desc = pair['description']
             open_time = desc[0]
             opens += name + ', ' + open_time + '\n'
+
         return opens
 
 class CloseCSV(Resource):
-    def get(self, top=0):
-        data = db.tododb.find()
+    def get(self):
+        if request.args.get('top'):  # if 'top' parameter was given
+            top = request.args.get('top')
+            top = int(top)
+            data = []
+            data = db.tododb.find().limit(top)
+
+        else:
+            data = db.tododb.find()
+
         closures = "km, close\n"  # column header
+
         for pair in data:
             name = pair['name']
             desc = pair['description']
             close_time = desc[1]
             closures += name + ', ' + close_time + '\n'
 
+        return closures
+
 # Create routes
 # Another way, without decorators
-api.add_resource(All, '/', '/listAll', '/listAll/json', '/?top=<int:top>', '/listAll?top=<int:top>', '/listAll/json?top=<int:top>', '/listAll/', '/listAll/json/')  # Adding option for '/' at the end in case the browser automatically adds it
+api.add_resource(All, '/', '/listAll', '/listAll/json', '/listAll/', '/listAll/json/')  # Adding option for '/' at the end in case the browser automatically adds it
 
 api.add_resource(Open, '/listOpenOnly', '/listOpenOnly/json', '/listOpenOnly?<int:top>', '/listOpenOnly/json?<int:top>', '/listOpenOnly/', '/listOpenOnly/json/')
 
